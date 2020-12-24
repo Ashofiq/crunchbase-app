9ef67d8b513042c7d68d3c87f968ffd8d5d5bc81
shofiq <ahmadshofiq3@gmail.com>
2020-12-24 10:56:49 +0600

    first commit

---
+++ crunchbase.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
+const port = 3001
+
+
+
+
+
+app.get('/', (req, res) => {
+  
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: false,
+            slowMo:250
+        });
+
+        const page = await browser.newPage();
+    
+        page.waitForNavigation({waitUntil: "domcontentloaded"});
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+        await page.goto("https://www.crunchbase.com/organization/amazon");
+
+
+        const title = await page.$$eval("#client-app-state", elements => {
+            return elements.map(item => item.textContent.replace(/\&q;/g, '"').replace('GET/v4/data/entities/organizations/amazonfield_ids=%5B%22identifier%22,%22layout_id%22,%22facet_ids%22,%22title%22,%22short_description%22,%22is_locked%22%5D&a;layout_mode=view_v2', "data"))
+        });
+        let m = JSON.parse(title);
+        let n = m.HttpState.data.text
+        
+        res.send(n).json();
+    
+        await browser.close();
+    
+    })();
+
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const port = 3001
+
+// const dom = new JSDOM(``, {
+//     url: "https://www.crunchbase.com/organization/twitter",
+//     referrer: "https://google.com/",
+//     contentType: "text/html",
+//     includeNodeLocations: true,
+//     storageQuota: 1000000000000
+//   });
+
+// console.log(dom.window.document.querySelector("#client-app-state").textContent);
+
+app.get('/:name', (req, res) => {
+
+    const name = req.params.name
+    JSDOM.fromURL("https://www.crunchbase.com/organization/"+name, 'text/html').then(dom => {
+        res.send(dom.window.document.querySelector("#client-app-state").text.replace(/\&q;/g, '"'));
+    });
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
